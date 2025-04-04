c211e7423a30ffaa7b01abeed2188d3ed433affc
Matthew Palmer <matthewpalmer2209@gmail.com>
2025-04-04 22:55:17 +0100

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+
+namespace BudgetBuddy;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddSyncfusionBlazor();
+
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mzc5ODE5NUAzMjM5MmUzMDJlMzAzYjMyMzkzYktkUE5KdDBMZXo1cURzM3RlRW9ka29xVmQ4cEdlTVpnQkp5Y1Q5UXM0NVE9");
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
