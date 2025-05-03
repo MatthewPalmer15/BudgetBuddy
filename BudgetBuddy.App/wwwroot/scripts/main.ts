ddabfbde01fa5850e62fc443a3a10b2865dad740
Matthew Palmer <matthewpalmer2209@gmail.com>
2025-05-03 21:56:05 +0100

    Managed to get js to run

--- BudgetBuddy.App/wwwroot/scripts/main.ts
+++ BudgetBuddy.App/wwwroot/scripts/main.ts
@@ -1,8 +1,21 @@
-import '../styles/main.scss';
+﻿import '../styles/main.scss';
 
 import "@preline/accordion";
 import "@preline/dropdown";
 import "@preline/overlay";
 import "@preline/carousel";
 import "@preline/stepper";
-import "preline/preline";
+import { createIcons, icons } from 'lucide';
+import 'preline/dist/preline.js';
+
+declare global {
+    interface Window {
+        marukiInit: () => void;
+    }
+}
+
+window.marukiInit = () => {
+    createIcons({ icons });
+    // @ts-ignore
+    window.HSStaticMethods?.autoInit?.();
+};
